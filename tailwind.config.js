bc4c00d4a528eba8fd59f397491833ee54e1826f
JiroKakpovbia <kakpovbia.jiro@gmail.com>
2024-12-28 17:06:18 -0500

    added animate on scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,32 +6,7 @@ module.exports = {
       sans: ['Gotham', 'Graphik', 'Helvetica'],
       serif: ['Georgia', 'Cambria'],
     },
-    extend: {
-      keyframes: {
-        slideInLeft: {
-          '0%': { transform: 'translateX(-100%)', opacity: '0' },
-          '100%': { transform: 'translateX(0)', opacity: '1' },
-        },
-        slideInRight: {
-          '0%': { transform: 'translateX(100%)', opacity: '0' },
-          '100%': { transform: 'translateX(0)', opacity: '1' },
-        },
-        slideInUp: {
-          '0%': { transform: 'translateY(100%)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
-        },
-        slideInDown: {
-          '0%': { transform: 'translateY(-100%)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
-        },
-      },
-      animation: {
-        slideInLeft: 'slideInLeft 1s ease-in-out',
-        slideInRight: 'slideInRight 1s ease-in-out',
-        slideInUp: 'slideInUp 1s ease-in-out',
-        slideInDown: 'slideInDown 1s ease-in-out',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
