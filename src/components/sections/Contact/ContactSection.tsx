1a0c652cb51dacd130d3edc21eb10bea58fa63e0
JiroKakpovbia <kakpovbia.jiro@gmail.com>
2025-12-13 15:10:52 -0500

    modified ids in contact section

--- src/components/sections/Contact/ContactSection.tsx
+++ src/components/sections/Contact/ContactSection.tsx
@@ -5,16 +5,15 @@ import ContactForm from "./components/ContactForm.tsx";
 
 const ContactInfoSection = () => {
 	const contactMethods = [
-		{ id: "phone", method: "Phone Number", label: "(226) 899-3921", url: "tel:2268993921", icon: "fa-solid fa-phone mb-4" },
-		{ id: "email", method: "Email", label: "kakpovbia.jiro@gmail.com", url: "mailto:kakpovbia.jiro@gmail.com", icon: "fa-regular fa-envelope mb-4" },
+		{ method: "Phone Number", label: "(226) 899-3921", url: "tel:2268993921", icon: "fa-solid fa-phone mb-4" },
+		{ method: "Email", label: "kakpovbia.jiro@gmail.com", url: "mailto:kakpovbia.jiro@gmail.com", icon: "fa-regular fa-envelope mb-4" },
 		{
-			id: "c-linkedin",
 			method: "LinkedIn",
 			label: "jiro-kakpovbia",
 			url: "https://linkedin.com/in/jiro-kakpovbia",
 			icon: "fa-brands fa-linkedin-in mb-4",
 		},
-		{ id: "c-github", method: "Github", label: "JiroKakpovbia", url: "https://github.com/JiroKakpovbia", icon: "fa-brands fa-github mb-4" },
+		{ method: "Github", label: "JiroKakpovbia", url: "https://github.com/JiroKakpovbia", icon: "fa-brands fa-github mb-4" },
 	];
 	return (
 		<section id="contact">
