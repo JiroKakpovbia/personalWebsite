47d75547f37317508a41f163f6b8443836ba0064
JiroKakpovbia <kakpovbia.jiro@gmail.com>
2025-12-13 15:10:27 -0500

    moved the grid in experience info block

--- src/components/sections/Experience/components/ExperienceInfoBlock.tsx
+++ src/components/sections/Experience/components/ExperienceInfoBlock.tsx
@@ -12,33 +12,33 @@ const ExperienceInfoBlock = ({ id, experience }: ExperienceInfoBlockProps) => {
 			<Grid container size={{ xs: 5, sm: 2, md: 2, lg: 0.8, xl: 0.8 }} id={`${id}-logo`}>
 				<img alt={`${experience.company} Logo`} src={experience.image} />
 			</Grid>
-			<Grid container size={{ xs: 12, sm: 10, md: 10, lg: 11, xl: 11.2 }} spacing={0} id={`${id}-content`}>
-				<Grid container size={12} justifyContent={"space-between"} className="job-title">
-					<h2 id={`${id}-position`} className="position">
-						{experience.position}
-					</h2>
-					<h2 id={`${id}-dates`} className="dates">
-						<span className="short">
-							{`${experience.startDate.split(" ")[0].substring(0, 3)} ${experience.startDate.split(" ")[1]} — ${
-								experience.endDate === "Present"
-									? "Present"
-									: experience.endDate.split(" ")[0].substring(0, 3) + " " + experience.endDate.split(" ")[1]
-							}`}
-						</span>
-						<span className="full">
-							{experience.startDate} — {experience.endDate}
-						</span>
-					</h2>
-				</Grid>
-				<Grid container size={12} spacing={1}>
+			<Grid container size={{ xs: 12, sm: 10, md: 10, lg: 11, xl: 11.2 }} spacing={1} id={`${id}-content`}>
+				<Grid container size={12} spacing={0}>
+					<Grid container size={12} justifyContent={"space-between"} className="job-title">
+						<h2 id={`${id}-position`} className="position">
+							{experience.position}
+						</h2>
+						<h2 id={`${id}-dates`} className="dates">
+							<span className="short">
+								{`${experience.startDate.split(" ")[0].substring(0, 3)} ${experience.startDate.split(" ")[1]} — ${
+									experience.endDate === "Present"
+										? "Present"
+										: experience.endDate.split(" ")[0].substring(0, 3) + " " + experience.endDate.split(" ")[1]
+								}`}
+							</span>
+							<span className="full">
+								{experience.startDate} — {experience.endDate}
+							</span>
+						</h2>
+					</Grid>
 					<Grid container size={12}>
 						<p id={`${id}-location`} className="location">
 							{experience.company}, {experience.location}
 						</p>
 					</Grid>
-					<Grid container size={12}>
-						<p id={`${id}-tools`} dangerouslySetInnerHTML={{ __html: experience.content }}></p>
-					</Grid>
+				</Grid>
+				<Grid container size={12}>
+					<p id={`${id}-tools`} dangerouslySetInnerHTML={{ __html: experience.content }}></p>
 				</Grid>
 			</Grid>
 		</Grid>
