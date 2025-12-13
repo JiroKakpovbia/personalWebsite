9a852362a0698da797734471fa4cc99b4db77224
JiroKakpovbia <kakpovbia.jiro@gmail.com>
2025-12-13 16:41:50 -0500

    converted navigation bar to use grids

--- src/components/sections/Navigation/NavigationSection.tsx
+++ src/components/sections/Navigation/NavigationSection.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./Navigation.css";
+import { Grid } from "@mui/material";
 
 interface NavigationSectionProps {
 	isNavScrolled: boolean;
@@ -15,60 +16,65 @@ const NavigationSection = ({ isNavScrolled, theme, toggleTheme, sections }: Navi
 	};
 
 	return (
-		<div>
+		<Grid container size={12}>
 			{/* Desktop Navigation */}
 			<nav id="desktop-nav" className={isNavScrolled ? "scrolled" : ""}>
-				<div className="logo" data-aos="fade-down" data-aos-once="true">
-					Jiro Kakpovbia
-				</div>
-				<div className="nav-actions" data-aos="fade-down" data-aos-once="true">
-					<ul className="nav-links">
+				<Grid container size={12} padding={3} justifyContent={"space-between"}>
+					{/* Name */}
+					<Grid container className={"logo"} data-aos="fade-down" data-aos-once="true">
+						<h2>Jiro Kakpovbia</h2>
+					</Grid>
+					{/* Page Links */}
+					<Grid container spacing={3} data-aos="fade-down" data-aos-once="true">
 						{sections.map((section) => {
 							return (
-								<li>
+								<Grid container className="nav-links">
 									<a href={`#${section.toLowerCase()}`}>{section}</a>
-								</li>
+								</Grid>
 							);
 						})}
-					</ul>
-
-					<button aria-label="theme-toggle" className="theme-toggle" onClick={toggleTheme}>
-						<i className={`fas ${theme === "light" ? "fa-sun" : "fa-moon"}`}></i>
-					</button>
-				</div>
+						<Grid container className="theme-toggle">
+							<button aria-label="theme-toggle" onClick={toggleTheme}>
+								<i className={`fas ${theme === "light" ? "fa-sun" : "fa-moon"}`}></i>
+							</button>
+						</Grid>
+					</Grid>
+				</Grid>
 			</nav>
 
 			{/* Hamburger Navigation */}
 			<nav id="hamburger-nav" className={isNavScrolled ? "scrolled" : ""}>
-				<div className="logo" data-aos="fade-down" data-aos-once="true">
-					Jiro Kakpovbia
-				</div>
-
-				<div className="nav-actions" data-aos="fade-down" data-aos-once="true">
-					<button aria-label="theme-toggle" className="theme-toggle" onClick={toggleTheme}>
-						<i className={`fas ${theme === "light" ? "fa-sun" : "fa-moon"}`}></i>
-					</button>
-					<div className="hamburger-menu">
-						<button className="hamburger-icon" aria-label="Navigation" onClick={handleMenuToggle}>
-							<span></span>
-							<span></span>
-							<span></span>
-						</button>
-						<ul className={`menu-links ${menuOpen ? "open" : ""}`}>
-							{sections.map((section) => {
-								return (
-									<li>
-										<a href={`#${section.toLowerCase()}`} onClick={handleMenuToggle}>
-											{section}
-										</a>
-									</li>
-								);
-							})}
-						</ul>
-					</div>
-				</div>
+				<Grid container size={12} padding={3} justifyContent={"space-between"}>
+					{/* Website Name */}
+					<Grid container className={"logo"} data-aos="fade-down" data-aos-once="true">
+						<h2>Jiro Kakpovbia</h2>
+					</Grid>
+					<Grid container spacing={3} data-aos="fade-down" data-aos-once="true">
+						<Grid container className="theme-toggle">
+							<button aria-label="theme-toggle" onClick={toggleTheme}>
+								<i className={`fas ${theme === "light" ? "fa-sun" : "fa-moon"}`}></i>
+							</button>
+						</Grid>
+						<Grid container className="hamburger-menu">
+							<button className="hamburger-icon" aria-label="Navigation" onClick={handleMenuToggle}>
+								<span></span>
+								<span></span>
+								<span></span>
+							</button>
+							<Grid container className={`menu-links ${menuOpen ? "open" : ""}`}>
+								{sections.map((section) => {
+									return (
+										<Grid container className="nav-links" onClick={handleMenuToggle}>
+											<a href={`#${section.toLowerCase()}`}>{section}</a>
+										</Grid>
+									);
+								})}
+							</Grid>
+						</Grid>
+					</Grid>
+				</Grid>
 			</nav>
-		</div>
+		</Grid>
 	);
 };
 
