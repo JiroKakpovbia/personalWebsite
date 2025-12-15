1b592780631dcf170f40b23469d09ff60c62a81c
JiroKakpovbia <kakpovbia.jiro@gmail.com>
2025-12-14 22:33:03 -0500

    converted other files to typescript

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+/** @type {import("tailwindcss").Config} */
+export const content = ["./src/**/*.{html,js}", "./public/index.html"];
+export const theme = {
+	fontFamily: {
+		sans: ["Gotham", "Graphik", "Helvetica"],
+		serif: ["Georgia", "Cambria"],
+	},
+	extend: {},
+};
+export const plugins = [];
